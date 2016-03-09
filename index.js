6debd4cfa8397de2daca9439c047b8b2cbb63f45
Skyler Layne <skylerclayne@gmail.com>
2016-03-09 11:42:19 -0500

    added bin entry point

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+#! /usr/bin/env node
+
 /*
  * Copyright (c) 2015, Skyler Layne. All rights reserved.
  * Copyrights licensed under the MIT License.
  * See the accompanying LICENSE file for terms.
  */
-
 module.exports = require("./src/cli");
