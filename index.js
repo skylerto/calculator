f302a1f8052f36766cdc18cc07cd22a1fa9971b4
Skyler Layne <skylerclayne@gmail.com>
2016-03-09 11:20:53 -0500

    add app entry point

---
+++ index.js
@@ -0,0 +1,7 @@
+/*
+ * Copyright (c) 2015, Skyler Layne. All rights reserved.
+ * Copyrights licensed under the MIT License.
+ * See the accompanying LICENSE file for terms.
+ */
+
+module.exports = require("./src/cli");
